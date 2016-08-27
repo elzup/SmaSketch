c8d44497e59149971fd6ce1ff2cd6ec5efa58dd3
elzup <guild0105@gmail.com>
2016-08-28 00:27:46 +0900

    Refactor Format

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,17 @@
-require('normalize.css/normalize.css');
-require('styles/App.css');
+require('normalize.css/normalize.css')
+require('styles/App.css')
 
-import React from 'react';
-
-let yeomanImage = require('../images/yeoman.png');
+import React from 'react'
 
 class AppComponent extends React.Component {
-  render() {
-    return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<canvas>
+			</canvas>
+		)
+	}
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {}
 
-export default AppComponent;
+export default AppComponent
