cb8fdb972e21024200061c77517f4b6350a02322
elzup <guild0105@gmail.com>
2016-08-29 04:46:18 +0900

    Create Sub draws

--- src/components/Sub.js
+++ src/components/Sub.js
@@ -1,10 +1,10 @@
+import React from 'react'
 import io from 'socket.io-client'
-import $ from 'jquery'
+import pos from 'dom.position'
 
 require('normalize.css/normalize.css')
 require('styles/App.css')
 
-import React from 'react'
 
 export default class SubComponent extends React.Component {
 	render() {
@@ -13,7 +13,7 @@ export default class SubComponent extends React.Component {
 				<h1 className="title_con">Sma Sketch</h1>
 				<div className="main">
 					<div className="canvas">
-						<canvas id="myCanvas" />
+						<canvas id="myCanvas"/>
 					</div>
 				</div>
 			</div>
@@ -25,58 +25,41 @@ export default class SubComponent extends React.Component {
 	componentDidMount() {
 
 		const stopDefault = (event) => {
-			if (event.touches[0].target.tagName.toLowerCase() == 'li') {
+			if (event.touches[0] && event.touches[0].target.tagName == 'button') {
 				return
 			}
-			if (event.touches[0].target.tagName.toLowerCase() == 'input') {
-				return
-			}
-
 			event.preventDefault()
 		}
 
-		document.addEventListener('touchstart', stopDefault, false)
-		document.addEventListener('touchmove', stopDefault, false)
-		document.addEventListener('touchend', stopDefault, false)
-		document.addEventListener('gesturestart', stopDefault, false)
-		document.addEventListener('gesturechange', stopDefault, false)
-		document.addEventListener('gestureend', stopDefault, false)
+		['touchstart', 'touchmove', 'touchend','gesturestart', 'gesturechange', 'gestureend'].forEach(func => {
+			document.addEventListener(func, stopDefault, false)
+		})
 
-		var socket = io.connect(window.location.hostname + ':8080')
+		const socket = io.connect(window.location.hostname + ':8080')
 
-		var canvas = document.getElementById('myCanvas')
-		var c = canvas.getContext('2d')
-		var drawing = false
-		var oldPos
+		const canvas = document.getElementById('myCanvas')
+		const c = canvas.getContext('2d')
+		let drawing = false
+		let oldPos
 
-		var w = window.innerWidth
-		var h = window.innerHeight
-		canvas.width = w - 20
-		canvas.height = h - 100
+		canvas.width = window.innerWidth - pos(canvas).left - 10
+		canvas.height = window.innerHeight - pos(canvas).top - 10
 
 		c.strokeStyle = '#000000'
 		c.lineWidth = 5
 		c.lineJoin = 'round'
 		c.lineCap = 'round'
 
-		function scrollX() {
-			return document.documentElement.scrollLeft || document.body.scrollLeft
-		}
-
-		function scrollY() {
-			return document.documentElement.scrollTop || document.body.scrollTop
-		}
-
 		const getPosMouse = (event) => {
-			var mouseX = event.clientX - $(canvas).position().left + scrollX()
-			var mouseY = event.clientY - $(canvas).position().top + scrollY()
+			const mouseX = event.clientX - pos(canvas).left
+			const mouseY = event.clientY - pos(canvas).top
 			return {x: mouseX, y: mouseY}
 		}
 
 		const getPosTouch = (event) => {
-			var mouseX = event.touches[0].clientX - $(canvas).position().left + scrollX()
-			var mouseY = event.touches[0].clientY - $(canvas).position().top + scrollY()
-			return {x:mouseX, y:mouseY}
+			const mouseX = event.touches[0].clientX - pos(canvas).left
+			const mouseY = event.touches[0].clientY - pos(canvas).top
+			return {x: mouseX, y: mouseY}
 		}
 
 		const touchStart = getPos => {
@@ -98,7 +81,7 @@ export default class SubComponent extends React.Component {
 
 		const touchMove = (getPos) => {
 			return (event) => {
-				var pos = getPos(event)
+				const pos = getPos(event)
 				console.log('mousemove : x=' + pos.x + ', y=' + pos.y + ', drawing=' + drawing)
 				if (drawing) {
 					c.beginPath()
@@ -119,35 +102,6 @@ export default class SubComponent extends React.Component {
 			drawing = false
 		}, false)
 
-		$('#black').click(() => {
-			c.strokeStyle = 'black'
-			socket.emit('color', 'black')
-		})
-		$('#blue').click(() => {
-			c.strokeStyle = 'blue'
-			socket.emit('color', 'blue')
-		})
-		$('#red').click(() => {
-			c.strokeStyle = 'red'
-			socket.emit('color', 'red')
-		})
-		$('#green').click(() => {
-			c.strokeStyle = 'green'
-			socket.emit('color', 'green')
-		})
-		$('#small').click(() => {
-			c.lineWidth = 5
-			socket.emit('lineWidth', 5)
-		})
-		$('#middle').click(() => {
-			c.lineWidth = 10
-			socket.emit('lineWidth', 10)
-		})
-		$('#large').click(() => {
-			c.lineWidth = 20
-			socket.emit('lineWidth', 20)
-		})
-
 		socket.on('draw', data => {
 			console.log('on draw : ' + data)
 			c.beginPath()
@@ -155,16 +109,6 @@ export default class SubComponent extends React.Component {
 			c.lineTo(data.after.x, data.after.y)
 			c.stroke()
 			c.closePath()
-		})
-
-		socket.on('color', data => {
-			console.log('on color : ' + data)
-			c.strokeStyle = data
-		})
-
-		socket.on('lineWidth', data => {
-			console.log('on lineWidth : ' + data)
-			c.lineWidth = data
 		})
 
 	}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import React from 'react'
 import {Router, Route, browserHistory} from 'react-router'
 import ReactDOM from 'react-dom'
 import MainComponent from 'components/Main'
+import SubComponent from 'components/Sub'
 
 // Render the main component into the dom
 ReactDOM.render((
 	<Router history={browserHistory}>
 		<Route path="/" component={MainComponent}/>
-		<Route path="sub" component={(MainComponent)}/>
+		<Route path="sub" component={(SubComponent)}/>
 	</Router>
 ), document.getElementById('app'))
