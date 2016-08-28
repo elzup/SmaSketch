951e110583696b7008d9267610a181b8c7686c7c
elzup <guild0105@gmail.com>
2016-08-29 03:24:47 +0900

    Add routing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import 'core-js/fn/object/assign'
 import React from 'react'
+import {Router, Route, browserHistory} from 'react-router'
 import ReactDOM from 'react-dom'
 import MainComponent from 'components/Main'
 
 // Render the main component into the dom
-ReactDOM.render(<MainComponent />, document.getElementById('app'))
+ReactDOM.render((
+	<Router history={browserHistory}>
+		<Route path="/" component={MainComponent}/>
+		<Route path="sub" component={(MainComponent)}/>
+	</Router>
+), document.getElementById('app'))
