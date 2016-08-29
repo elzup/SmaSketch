caf4eaf6e6b62ec1e20f800984c61c90374f8486
elzup <guild0105@gmail.com>
2016-08-29 22:23:10 +0900

    Add edit mode

--- cfg/dist.js
+++ cfg/dist.js
@@ -39,4 +39,14 @@ config.module.loaders.push({
 	)
 })
 
+config.module.loaders.push({
+	test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+	loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+})
+
+config.module.loaders.push({
+	test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+	loader: 'file-loader'
+})
+
 module.exports = config
--- src/components/Sub.js
+++ src/components/Sub.js
@@ -2,17 +2,25 @@ import React from 'react'
 import io from 'socket.io-client'
 import pos from 'dom.position'
 import queryString from 'query-string'
+import {Icon} from 'react-fa'
 
 require('normalize.css/normalize.css')
 require('styles/App.css')
 
 
 export default class SubComponent extends React.Component {
-
 	render() {
 		return (
 			<div>
-				<h1 className="title_con">Sma Sketch</h1>
+				<div className="head">
+					<h1 className="title_con">Sma Sketch</h1>
+					<div>
+						<input type="radio" name="mode" id="pencil"/>
+						<Icon name="pencil"/>
+						<input type="radio" name="mode" id="eraser"/>
+						<Icon name="eraser"/>
+					</div>
+				</div>
 				<div className="main">
 					<div className="canvas">
 						<canvas id="myCanvas"/>
@@ -22,22 +30,38 @@ export default class SubComponent extends React.Component {
 		)
 	}
 
-	static defaultComponent = {}
-
 	componentDidMount() {
-
 		const stopDefault = (event) => {
-			if (event.touches[0] && event.touches[0].target.tagName == 'button') {
+			if (['input', 'button'].includes(event.touches[0] && event.touches[0].target.tagName.toLowerCase())) {
+				console.log('in')
 				return
 			}
+			console.log('out')
 			event.preventDefault()
 		}
-
 		['touchstart', 'touchmove', 'touchend', 'gesturestart', 'gesturechange', 'gestureend'].forEach(func => {
 			document.addEventListener(func, stopDefault, false)
 		})
 
 		const socket = io.connect(window.location.hostname + ':8080')
+
+		let isPencilMode = true
+
+		const pencil = document.getElementById('pencil')
+		const eraser = document.getElementById('eraser')
+		const touchPencil = () => {
+			isPencilMode = true
+			pencil.click()
+		}
+		pencil.addEventListener('mousedown', touchPencil, false)
+		pencil.addEventListener('touchstart', touchPencil, false)
+		const touchEraser = () => {
+			isPencilMode = false
+			eraser.click()
+		}
+		eraser.addEventListener('mousedown', touchEraser, false)
+		eraser.addEventListener('touchstart', touchEraser, false)
+		pencil.click()
 
 		const canvas = document.getElementById('myCanvas')
 		const c = canvas.getContext('2d')
@@ -103,6 +127,7 @@ export default class SubComponent extends React.Component {
 				const pos = getPos(event)
 				console.log('mousemove : x=' + pos.x + ', y=' + pos.y + ', drawing=' + drawing)
 				if (drawing) {
+					c.strokeStyle = '#000000'
 					c.beginPath()
 					c.moveTo(oldPos.x, oldPos.y)
 					c.lineTo(pos.x, pos.y)
